bc49fbc9997238d7e0a69e02009d4a70e37cf390
Kevin N <kevin.norris.90@gmail.com>
2017-04-05 17:51:41 +1200

    Add basic stock data functionality

--- server/src/index.js
+++ server/src/index.js
@@ -1,20 +1,72 @@
 import WebSocket from 'ws';
 import http from 'http';
+import 'es6-promise/auto';
+import 'isomorphic-fetch';
+import 'dotenv/config';
 import app from './http-server';
 
-// https://www.quandl.com/api/v3/datasets/WIKI/FB/data.json?api_key=QD8yWR8zHJs_27sSZuzC&column_index=4
-// https://www.quandl.com/api/v3/datasets/WIKI/FB/data.json?api_key=QD8yWR8zHJs_27sSZuzC&column_index=4&transform=rdiff
-
+// https://www.quandl.com/api/v3/datasets/WIKI/FB/data.json?api_key=${process.env.QUANDL_API_KEY}&column_index=4
+// https://www.quandl.com/api/v3/datasets/WIKI/FB/data.json?api_key=${process.env.QUANDL_API_KEY}&column_index=4&transform=rdiff
+// https://www.quandl.com/api/v3/datatables/WIKI/PRICES.json?ticker=MSFT,YHOO&qopts.columns=date,open&api_key=${process.env.QUANDL_API_KEY}
+// Could do combined calls with 3rd api call
+// but since has max of 10k rows, even 2 older companies data would not fit in one response
 const server = http.createServer(app);
 const wss = new WebSocket.Server({server});
 
-wss.on('connection', (ws) => {
-  ws.on('message', (message) => {
-    console.log(`recieved: ${message}`);
+// Stocks viewed by all users, array of {name, data}
+let stocks = [];
 
-    ws.send(JSON.stringify({
-      answer: 42,
-    }));
+const getStockData = (name, callback) => {
+  const url = `https://www.quandl.com/api/v3/datasets/WIKI/${name}` +
+    `/data.json?api_key=${process.env.QUANDL_API_KEY}&column_index=4`;
+
+  fetch(url)
+    .then(response => response.json())
+    .then((json) => {
+      if (json.quandl_error) {
+        callback(json);
+      } else {
+        callback(json.dataset_data.data);
+      }
+    }).catch((err) => {
+      callback({error: err.message});
+    });
+};
+
+getStockData('FB', (data) => {
+  if (data.error) {
+    console.log(data.error);
+  } else {
+    console.log('data recieved');
+    stocks.push({
+      name: 'FB',
+      data,
+    });
+  }
+});
+
+wss.on('connection', (ws) => {
+  // Send current stock data
+  ws.send(JSON.stringify({type: 'StockData', seriesData: stocks}));
+
+  ws.on('getStock', (name) => {
+    const nameUpper = name.toUpperCase();
+    console.log(`getting stock: ${nameUpper}`);
+    // If name not already in stocks
+    if (stocks.indexOf(nameUpper) === -1) {
+      getStockData(nameUpper, (data) => {
+        if (data.error) {
+          console.log(`error occured: ${data.error}`);
+          // ws.send(JSON.stringify(data));
+        } else {
+          console.log('recieved data:', data);
+          // Format and push to stocks
+          // push to all users
+        }
+      });
+    } else {
+      ws.send(JSON.stringify({error: `${nameUpper} is not a valid symbol name`}));
+    }
   });
 });
 
