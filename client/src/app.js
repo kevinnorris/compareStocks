bfab33295f5dbd64121cc6594adfee84353a88fc
Kevin N <kevin.norris.90@gmail.com>
2017-04-10 08:22:03 +1200

    Replace static host string with dynamic

--- client/src/app.js
+++ client/src/app.js
@@ -179,7 +179,8 @@ document.body.onload = () => {
     Websocket code
     ----------------------------
   */
-  const socket = new WebSocket('ws://localhost:8080/');
+  const HOST = location.origin.replace(/^http/, 'ws');
+  const socket = new WebSocket(HOST);
 
   const removeMessage = name => (
     () => {
